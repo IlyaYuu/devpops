a76f29cbaee35ec648a9404a9278b59aa2299853
IliaYuu <ilililya283@gmail.com>
2022-11-23 22:43:08 +0200

    fix pass

--- lab4/script.sh
+++ lab4/script.sh
@@ -1,5 +1,5 @@
 sudo useradd -m adminuser
-echo adminuser | passwd adminuser --stdin
+echo 'adminuser:123' | chpasswd # change user adminuser password to newpassword 
 sudo usermod -a -G sudo adminuser #sudo get
 # sudo passwd adminuser 123
 
@@ -8,7 +8,7 @@ sudo useradd -m poweruser
 sudo passwd -d poweruser #del pass
 usermod -a -G sudo poweruser #sudo get
 echo 'poweruser ALL=(ALL:ALL) /usr/sbin/iptables' >> /etc/sudoers
-#sudo iptables -L # open uptables
+ln -s /home/poweruser/etc/mtab softlink # open uptables
 
 # iptables -t filter --append INPUT -j DROP
 
